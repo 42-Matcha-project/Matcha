fc35db06397ea4195f9467412be0af6518715dec
kkodaira <kodaira.k55@gmail.com>
2025-02-09 23:07:48 +0900

    [add] 実際にGoでクエリを実行し結果を出力する

--- playground/kkodaira/go-and-mysql/go/src/data-access/main.go
+++ playground/kkodaira/go-and-mysql/go/src/data-access/main.go
@@ -9,6 +9,58 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
+// nameからそのアーティストのアルバムを検索する
+func albumsByArtist(name string) ([]Album, error) {
+	var albums []Album
+
+	// db.Queryでクエリを送れる。?はプレースホルダでnameを挿入する。これによりSQLインジェクションを防げる
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	// rows()データベースクエリ結果は使用後閉じなきゃいけない
+	defer rows.Close()
+
+	// 結果の全ての行をループ
+	for rows.Next() {
+		var alb Album
+		// Scan
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	// クエリ実行中のエラー
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+
+	return albums, nil
+}
+
+func albumByID(id int64) (Album, error) {
+	var alb Album
+
+	// 指定したidの行を参照する
+	row := db.QueryRow("SELECT  * FROM album WHERE id = ?", id)
+	// クエリ結果がないときや変換エラー
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		// 当該するデータがない
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+	return alb, nil
+}
+
 var db *sql.DB
 
 func main() {
@@ -32,4 +84,16 @@ func main() {
 		log.Fatal(pingErr)
 	}
 	fmt.Println("Connected!")
+
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+
+	alb, err := albumByID(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", alb)
 }
