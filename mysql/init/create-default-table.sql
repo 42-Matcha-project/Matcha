2c737844497503b69930b181c2c076a6ea754492
kkodaira <kodaira.k55@gmail.com>
2025-02-10 23:18:07 +0900

    [update] コンテナ起動時にデフォルトで実際にusersテーブルを作成し、テストデータを挿入するようにした。また、微妙にdatabase.dbmlを修正した。

--- mysql/init/create-default-table.sql
+++ mysql/init/create-default-table.sql
@@ -1,16 +1,20 @@
-DROP TABLE IF EXISTS album;
-CREATE TABLE album (
-                       id         INT AUTO_INCREMENT NOT NULL,
-                       title      VARCHAR(128) NOT NULL,
-                       artist     VARCHAR(255) NOT NULL,
-                       price      DECIMAL(5,2) NOT NULL,
-                       PRIMARY KEY (`id`)
+CREATE DATABASE IF NOT EXISTS matcha;
+USE matcha;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id              INT AUTO_INCREMENT NOT NULL,
+    username        VARCHAR(20) NOT NULL UNIQUE,
+    email           VARCHAR(255) NOT NULL UNIQUE,
+    password        CHAR(60) NOT NULL,
+    gender          ENUM('male', 'female') NOT NULL,
+    introduction    VARCHAR(255),
+    icon_image_url  VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
 );
 
-INSERT INTO album
-(title, artist, price)
+INSERT INTO users (username, email, password, gender, introduction, icon_image_url)
 VALUES
-    ('Blue Train', 'John Coltrane', 56.99),
-    ('Giant Steps', 'John Coltrane', 63.99),
-    ('Jeru', 'Gerry Mulligan', 17.99),
-    ('Sarah Vaughan', 'Sarah Vaughan', 34.98);
+    ('kai', 'kai@example.com', 'hashed_password_1', 'male', 'Hello, I am Kai.', 'https://example.com/kai.png');
