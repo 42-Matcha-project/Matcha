0e8e0b9858d022091003140f1077029e83851a21
kkodaira <kodaira.k55@gmail.com>
2025-02-11 19:58:04 +0900

    [update] mysqlがコンテナ作成時に.envで指定したユーザーを追加するように設定した

--- db-mysql/entry-point.sh
+++ db-mysql/entry-point.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mysqld
-
-mysql -u root -p ${MYSQL_ROOT_PASSWORD} -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;"
+echo "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';" >> /docker-entrypoint-initdb.d/create-default-table.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'localhost' WITH GRANT OPTION;" >> /docker-entrypoint-initdb.d/create-default-table.sql
+echo "FLUSH PRIVILEGES;" >> /docker-entrypoint-initdb.d/create-default-table.sql
