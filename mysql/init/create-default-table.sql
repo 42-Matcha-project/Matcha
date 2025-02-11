0a840479b322529a53019f2cc79ff395e4711671
kkodaira <kodaira.k55@gmail.com>
2025-02-11 12:54:40 +0900

    [update] テーブルにusername(一意に定まるべき)とは別にdisplay_nameを追加した。表示名は一意じゃなくて良い。

--- mysql/init/create-default-table.sql
+++ mysql/init/create-default-table.sql
@@ -4,17 +4,18 @@ USE matcha;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id              INT AUTO_INCREMENT NOT NULL,
-    username        VARCHAR(20) NOT NULL UNIQUE,
+    username        VARCHAR(30) NOT NULL UNIQUE,
     email           VARCHAR(255) NOT NULL UNIQUE,
     password        CHAR(60) NOT NULL,
+    display_name    VARCHAR(20) NOT NULL ,
     gender          ENUM('male', 'female') NOT NULL,
     introduction    VARCHAR(255),
     icon_image_url  VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
 
-INSERT INTO users (username, email, password, gender, introduction, icon_image_url)
+INSERT INTO users (username, email, password, display_name, gender, introduction, icon_image_url)
 VALUES
-    ('kai', 'kai@example.com', 'hashed_password_1', 'male', 'Hello, I am Kai.', 'https://example.com/kai.png');
+    ('@kaikodaira1028', 'kai@example.com', 'hashed_password_1', 'Kai', 'male', 'Hello, I am Kai.', 'https://example.com/kai.png');
