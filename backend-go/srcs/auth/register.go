9d17b3879d117b4e455ff00aca67395625d588e2
kkodaira <kodaira.k55@gmail.com>
2025-02-17 18:00:14 +0900

    [update] register関数を大元にして、registerUserとregisterAffiliation関数に分けた

--- backend-go/srcs/auth/register.go
+++ backend-go/srcs/auth/register.go
@@ -24,7 +24,55 @@ type RegisterInput struct {
 	SexualPreferences []string `json:"SexualPreferences"`
 }
 
-func RegisterUser(reqContext *gin.Context) {
+func registerUser(registerInput RegisterInput) (*models.TUser, error) {
+	/*
+		UserをDBに保存する関数。
+	*/
+	registerUser := &models.TUser{
+		Username:     registerInput.Username,
+		Email:        registerInput.Email,
+		Password:     registerInput.Password,
+		DisplayName:  registerInput.DisplayName,
+		Gender:       registerInput.Gender,
+		Introduction: registerInput.Introduction,
+		IconImageURL: registerInput.IconImageUrl,
+	}
+
+	var err error
+	registerUser, err = registerUser.CreateUser()
+	return registerUser, err
+}
+
+func registerAffiliations(registerInput RegisterInput, registerUserID int) error {
+	/*
+		AffiliationをDBに保存する関数。
+		Affiliationの配列を受け取りそれらを各要素ごとにDBに保存する。
+	*/
+	var err error
+	if registerInput.Affiliations != nil && len(registerInput.Affiliations) != 0 {
+		for i := 0; i < len(registerInput.Affiliations); i++ {
+			affiliation := &models.TAffiliation{
+				Affiliation: registerInput.Affiliations[i],
+			}
+			affiliation, err = affiliation.CreateAffiliation()
+			if err != nil {
+				return err
+			}
+
+			user_affliation := &models.TUserAffiliation{
+				UserID:        registerUserID,
+				AffiliationID: affiliation.ID,
+			}
+			user_affliation, err = user_affliation.CreateUserAffiliation()
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func Register(reqContext *gin.Context) {
 	/*
 		新規ユーザーを登録する関数。
 		リクエストからJSONデータを抽出してDBに保存する。
@@ -39,49 +87,20 @@ func RegisterUser(reqContext *gin.Context) {
 		return
 	}
 
-	registerUser := &models.TUser{
-		Username:     registerInput.Username,
-		Email:        registerInput.Email,
-		Password:     registerInput.Password,
-		DisplayName:  registerInput.DisplayName,
-		Gender:       registerInput.Gender,
-		Introduction: registerInput.Introduction,
-		IconImageURL: registerInput.IconImageUrl,
-	}
-
-	registerUser, err := registerUser.CreateUser()
+	user, err := registerUser(registerInput)
 	if err != nil {
 		reqContext.JSON(http.StatusBadRequest, gin.H{"error": "Failed to create user"})
 		reqContext.Error(err)
 		return
 	}
 
-	if registerInput.Affiliations != nil && len(registerInput.Affiliations) != 0 {
-		for i := 0; i < len(registerInput.Affiliations); i++ {
-			affiliation := &models.TAffiliation{
-				Affiliation: registerInput.Affiliations[i],
-			}
-			affiliation, err = affiliation.CreateAffiliation()
-			if err != nil {
-				reqContext.JSON(http.StatusBadRequest, gin.H{"error": "Failed to create affiliation"})
-				reqContext.Error(err)
-				return
-			}
-
-			user_affliation := &models.TUserAffiliation{
-				UserID:        registerUser.ID,
-				AffiliationID: affiliation.ID,
-			}
-			user_affliation, err = user_affliation.CreateUserAffiliation()
-			if err != nil {
-				reqContext.JSON(http.StatusBadRequest, gin.H{"error": "Failed to create user_affiliation"})
-				reqContext.Error(err)
-				return
-			}
-		}
+	if err = registerAffiliations(registerInput, user.ID); err != nil {
+		reqContext.JSON(http.StatusBadRequest, gin.H{"error": "Failed to create affiliation"})
+		reqContext.Error(err)
+		return
 	}
 
 	reqContext.JSON(http.StatusOK, gin.H{
-		"user": registerUser.PrepareOutput(),
+		"user": user.PrepareOutput(),
 	})
 }
