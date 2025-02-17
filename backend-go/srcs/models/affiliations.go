06a24bbead74c79f6ab3571085a148bfdbca4f7d
kkodaira <kodaira.k55@gmail.com>
2025-02-17 16:13:26 +0900

    [add] t_affiliationsとt_user_affiliationsテーブル作成の関数

---
+++ backend-go/srcs/models/affiliations.go
@@ -0,0 +1,56 @@
+package models
+
+import "gorm.io/gorm"
+
+type TAffiliation struct {
+	/*
+		t_affiliationsテーブルの構造体
+	*/
+	ID          int    `gorm:"primaryKey;autoIncrement;column:id"`
+	Affiliation string `gorm:"type:varchar(100);not null;unique;gorm:column:affiliation"`
+}
+
+func (TAffiliation) TableName() string { return "t_affiliations" }
+
+type TUserAffiliation struct {
+	/*
+		t_user_affiliationsテーブルの構造体
+		t_userとt_affiliationsの中間テーブル
+	*/
+	ID            int    `gorm:"primaryKey;autoIncrement;column:id"`
+	UserID        int    `gorm:"type:int(10);not null;column:user_id"`
+	AffiliationID string `gorm:"type:int(10);not null;column:affiliation_id"`
+}
+
+func (TUserAffiliation) TableName() string { return "t_user_affiliations" }
+
+func (affiliation *TAffiliation) CreateAffiliation() (*TAffiliation, error) {
+	/*
+		DBに新規所属を保存する関数。
+		affiliationはuniqueなのでCreateの前にチェック
+	*/
+	err := DB.Where("affiliation = ?", affiliation.Affiliation).First(affiliation).Error
+	if err == nil {
+		return affiliation, nil
+	}
+	if err == gorm.ErrRecordNotFound {
+		err = DB.Create(affiliation).Error
+		if err != nil {
+			return nil, err
+		}
+		return affiliation, nil
+	}
+
+	return nil, err
+}
+
+func (user_affiliation *TUserAffiliation) CreateAffiliation() (*TUserAffiliation, error) {
+	/*
+		DBにユーザーと所属の中間テーブルの列を保存する関数。
+	*/
+	err := DB.Create(user_affiliation).Error
+	if err != nil {
+		return nil, err
+	}
+	return user_affiliation, nil
+}
