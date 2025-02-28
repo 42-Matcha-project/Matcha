d1fde189ab519e302abb8bae293b9c5aa79a9378
kkodaira <kodaira.k55@gmail.com>
2025-02-28 17:03:27 +0900

    [update] ManyToManyを双方向から設定

--- backend-go/srcs/models/affiliations.go
+++ backend-go/srcs/models/affiliations.go
@@ -9,8 +9,9 @@ type TAffiliation struct {
 	/*
 		t_affiliationsテーブルの構造体
 	*/
-	ID   int    `gorm:"primaryKey;autoIncrement;column:id"`
-	Name string `gorm:"type:varchar(100);not null;unique;gorm:column:name"`
+	ID     int     `gorm:"primaryKey;autoIncrement;column:id"`
+	Name   string  `gorm:"type:varchar(100);not null;unique;gorm:column:name"`
+	TUsers []TUser `gorm:"many2many:t_user_affiliations;"`
 }
 
 func (TAffiliation) TableName() string { return "t_affiliations" }
