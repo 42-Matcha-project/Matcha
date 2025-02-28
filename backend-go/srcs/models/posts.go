65f059e78cc0e2dd440bd9b930338c114631797a
kkodaira <kodaira.k55@gmail.com>
2025-02-28 15:01:24 +0900

    [update] 列制約の最適化

--- backend-go/srcs/models/posts.go
+++ backend-go/srcs/models/posts.go
@@ -12,7 +12,7 @@ type TPost struct {
 	ID        int       `gorm:"primaryKey;autoIncrement;column:id"`
 	UserID    uint      `gorm:"type:int(10);not null;column:user_id"`
 	Text      string    `gorm:"type:varchar(30);column:text"`
-	IsDraft   bool      `gorm:"type:boolean;column:is_draft"`
+	IsDraft   bool      `gorm:"type:boolean;not null;column:is_draft"`
 	CreatedAt time.Time `gorm:"column:created_at"`
 }
 
