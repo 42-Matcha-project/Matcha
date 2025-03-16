fab065512d79bfed3b184a624d2ae2061a7d3d99
kkodaira <kodaira.k55@gmail.com>
2025-03-16 21:25:35 +0900

    [Update] work_logsテーブルに作業時間(分間)を追加した。

--- backend-go/srcs/models/work_logs.go
+++ backend-go/srcs/models/work_logs.go
@@ -7,6 +7,7 @@ type TWorkLog struct {
 	UserID  int       `gorm:"type:int;not null;column:user_id" json:"-"`
 	WorkID  int       `gorm:"type:int;not null;column:work_id" json:"-"`
 	StartAt time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP;column:start_at"`
+	Minutes int64     `gorm:"type:bigint;not null;column:minutes"`
 	User    TUser     `gorm:"foreignKey:UserId;references:ID" json:"-"`
 	Work    TWork     `gorm:"foreignKey:WorkId;references:ID" json:"-"`
 }
