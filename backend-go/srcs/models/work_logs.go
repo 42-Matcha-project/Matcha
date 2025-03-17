ee3316765428b3df39130fc903341666a01389c3
kkodaira <kodaira.k55@gmail.com>
2025-03-17 16:51:42 +0900

    [Update] GORMのテーブル依存関係をより詳細に明示的に記述するようにした。

--- backend-go/srcs/models/work_logs.go
+++ backend-go/srcs/models/work_logs.go
@@ -8,8 +8,8 @@ type TWorkLog struct {
 	WorkID  int       `gorm:"type:int;not null;column:work_id" json:"-"`
 	StartAt time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP;column:start_at"`
 	Minutes int64     `gorm:"type:bigint;not null;column:minutes"`
-	User    TUser     `gorm:"foreignKey:UserId;references:ID" json:"-"`
-	Work    TWork     `gorm:"foreignKey:WorkId;references:ID" json:"-"`
+	User    TUser     `gorm:"foreignKey:UserID;references:ID" json:"-"`
+	Work    TWork     `gorm:"foreignKey:WorkID;references:ID" json:"-"`
 }
 
 func (*TWorkLog) TableName() string { return "work_logs" }
--- backend-go/srcs/models/works.go
+++ backend-go/srcs/models/works.go
@@ -4,11 +4,12 @@ type TWork struct {
 	/*
 		worksテーブルの構造体
 	*/
-	ID           int    `gorm:"primaryKey;autoIncrement;colum:id"`
-	UserID       int    `gorm:"type:int;not null;column:user_id" json:"-"`
-	WorkName     string `gorm:"type:varchar(60);not null;column:work_name"`
-	IconImageURL string `gorm:"type:varchar(255);column:icon_image_url"`
-	User         TUser  `gorm:"foreignKey:UserID;references:ID" json:"-"`
+	ID           int        `gorm:"primaryKey;autoIncrement;colum:id"`
+	UserID       int        `gorm:"type:int;not null;column:user_id" json:"-"`
+	WorkName     string     `gorm:"type:varchar(60);not null;column:work_name"`
+	IconImageURL string     `gorm:"type:varchar(255);column:icon_image_url"`
+	User         TUser      `gorm:"foreignKey:UserID;references:ID" json:"-"`
+	WorkLogs     []TWorkLog `gorm:"foreignKey:WorkID;references:ID" json:"-"`
 }
 
 func (*TWork) TableName() string {
