f2eb574197b9d8c079ef97f4d234583fad5fa96a
kkodaira <kodaira.k55@gmail.com>
2025-04-26 16:38:45 +0900

    [Update] フレンド申請をユーザー名で行うようにした。

--- backend-go/srcs/applogs/messages.go
+++ backend-go/srcs/applogs/messages.go
@@ -45,6 +45,7 @@ const (
 	EmailForUserAlreadyExists      = 111
 	UserNameForUserAlreadyExists   = 112
 	NotEnoughCoins                 = 113
+	OtherUserNotFound              = 114
 	// 200~ フロントエンドレベル
 	InvalidJSONInput            = 200
 	UserDoesNotOwnBuilding      = 201
@@ -80,6 +81,7 @@ const (
 	FailedToCreateBuilding       = 414
 	FailedToCreateWorkLog        = 415
 	FailedToSaveUser             = 416
+	FailedToGetUser              = 417
 )
 
 var Message = map[int]string{}
@@ -129,6 +131,7 @@ func init() {
 	Message[EmailForUserAlreadyExists] = "入力されたメールアドレスはすでに使用されています。ログインを試みてください。"
 	Message[UserNameForUserAlreadyExists] = "入力されたユーザー名はすでに使用されています。"
 	Message[NotEnoughCoins] = "コインが不足しています。"
+	Message[OtherUserNotFound] = "ユーザーが見つかりませんでした。"
 	// 200~ フロントエンドレベル
 	Message[InvalidJSONInput] = "JSONデータの形式にエラーがあります。"
 	Message[UserDoesNotOwnBuilding] = "ユーザーは建物を所有していません。"
@@ -163,4 +166,5 @@ func init() {
 	Message[FailedToCreateBuilding] = "データベースに建物を保存できませんでした。"
 	Message[FailedToCreateWorkLog] = "データベースにタスクログを保存できませんでした。"
 	Message[FailedToSaveUser] = "データベースのユーザー情報を更新できませんでした。"
+	Message[FailedToGetUser] = "データベースからユーザーの取得に失敗しました。"
 }
